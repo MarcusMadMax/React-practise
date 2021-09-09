375b4dc740da21e9c60d17268eeb5bf63527dcc0
MarcusMadMax <you@example.com>
2021-09-10 11:13:37 +1200

    added files

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h4>The Footer</h4>
+            <ul>
+                <li><a href="/#">Home</a></li>
+                <li><a href="/#">About</a></li>
+                <li><a href="/#">Contact</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>The Header</h1>
+            <ul>
+                <li><a href="/#">Home</a></li>
+                <li><a href="/#">About</a></li>
+                <li><a href="/#">Contact</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
