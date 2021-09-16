8ae70f8c46132541010c62f0fec9d5bac53fb363
MarcusMadMax <marcuszillner@icloud.com>
2021-09-16 23:05:36 +1200

    Created AppTest

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react'
 import Product from './Product'
 import productsData from './productsData'
 
-const App = () => {
-  const productComponent = productsData.map((item) => {
+class App extends React.Component {
+  render() {
+    const productComponent = productsData.map((item) => {
+      return (
+        <Product key={item.id} product={item} />
+      )
+    })
     return (
-      <Product key={item.id} product={item} />
+      <div className='wrapper'>
+        {productComponent}
+      </div>
     )
-  })
-  return (
-    <div className='wrapper'>
-      {productComponent}
-    </div>
-  )
+  }
 }
 
 export default App
