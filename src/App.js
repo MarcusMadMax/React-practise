f22b7e526de4bdb8d62059a49f7ce0d57d56a159
MarcusMadMax <you@example.com>
2021-09-21 21:10:41 +1200

    its a mess

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import TodosData from './todosData'
 import TodoItem from './TodoItem'
 
 class App extends React.Component {
-<<<<<<< HEAD
   constructor() {
     super()
     this.state = {
@@ -14,24 +13,18 @@ class App extends React.Component {
     const todosItem = this.state.todos.map((item) => {
       return (
         <TodoItem key={item.id} item={item} />
-=======
-  render() {
-    const productComponent = productsData.map((item) => {
-      return (
-        <Product key={item.id} product={item} />
->>>>>>> 8ae70f8c46132541010c62f0fec9d5bac53fb363
-      )
-    })
-    return (
-      <div className='wrapper'>
-<<<<<<< HEAD
-        {todosItem}
-=======
-        {productComponent}
->>>>>>> 8ae70f8c46132541010c62f0fec9d5bac53fb363
-      </div>
-    )
-  }
-}
+    render() {
+        const productComponent = productsData.map((item) => {
+          return (
+            <Product key={item.id} product={item} />
+          )
+        })
+        return (
+          <div className='wrapper'>
+            {todosItem}
+          </div>
+        )
+      }
+    }
 
 export default App
