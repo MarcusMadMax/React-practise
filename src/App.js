23bb1dffdf8cb4092a85054d42de08f1abd93360
MarcusMadMax <you@example.com>
2021-09-27 21:41:35 +1300

    just a bit

--- src/App.js
+++ src/App.js
@@ -9,9 +9,20 @@ class App extends React.Component {
     this.state = {
       todosData: TodosData
     }
+    this.handleClick = this.handleClick.bind(this)
   }
   handleClick(id) {
-    console.log('Clicked!', id)
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todosData.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+      return {
+        todosData: updatedTodos
+      }
+    })
   }
   render() {
     let todos = todosData.map((item) => {
