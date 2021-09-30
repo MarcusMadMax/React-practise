0b86fe91e907c31f254990f0cd872ad3728f9e62
MarcusMadMax <marcuszillner@icloud.com>
2021-09-30 22:45:00 +1300

    more

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import React from 'react'
-import TodosData from './todosData'
-import TodosItem from './TodoItem'
 import todosData from './todosData'
+import TodosItem from './TodoItem'
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      todosData: TodosData
+      todos: todosData
     }
     this.handleClick = this.handleClick.bind(this)
   }
   handleClick(id) {
-    this.setState((prevState) => {
-      const updatedTodos = prevState.todosData.map((todo) => {
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
         if (todo.id === id) {
           todo.completed = !todo.completed
         }
         return todo
       })
+      console.log(updatedTodos)
       return {
-        todosData: updatedTodos
+        todos: updatedTodos
       }
     })
   }
   render() {
-    let todos = todosData.map((item) => {
+    let todo = this.state.todos.map((item) => {
       return (
         <TodosItem key={item.id} item={item} handleClick={this.handleClick} />
       )
     })
     return (
       <div className="wrapper">
-        {todos}
+        {todo}
       </div>
     )
   }
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const TodoItem = (props) => {
+function TodoItem(props) {
     return (
         <div className="todos">
-            <input type="checkbox" checked={props.item.completed} onChange={() => props.handleClick(props.item.id)} />
+            <input
+                type="checkbox"
+                checked={props.item.completed}
+                onChange={() => props.handleClick(props.item.id)} />
             <h1>{props.item.text}</h1>
         </div>
     )
